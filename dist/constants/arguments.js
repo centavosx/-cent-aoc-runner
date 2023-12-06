ae22329435221d4dc4ad9df37567902290bb26af
Vincent Lennuel Llanto <vllanto@vllanto.local>
2023-12-06 14:42:05 +0800

    Removed dist

--- dist/constants/arguments.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommandArgs = void 0;
-exports.CommandArgs = {
-    "--path": "path",
-    "--part": "part"
-};
--- dist/implementation/aoc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AOC = void 0;
-class AOC {
-    part1(value) { }
-    part2(value) { }
-    runnerParser(value) { }
-}
-exports.AOC = AOC;
