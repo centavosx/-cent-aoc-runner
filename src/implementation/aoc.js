798353e38b2672ba7520c13807ab6bfefcc279b1
Vincent Lennuel Llanto <vllanto@vllanto.local>
2023-12-06 14:43:26 +0800

    Build js and export

---
+++ src/implementation/aoc.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AOC = void 0;
+class AOC {
+    part1(value) { }
+    part2(value) { }
+    runnerParser(value) { }
+}
+exports.AOC = AOC;
